a26cfb90d7aee8c894f2a6726caf1e8b9251b0a1
Ivan Bumbuc <bumbukk.bumbuk@gmail.com>
2023-03-20 00:10:23 +0200

    History Location

--- app/src/main/java/com/example/yoursafetyandroid/menu/HistoryFragment.java
+++ app/src/main/java/com/example/yoursafetyandroid/menu/HistoryFragment.java
@@ -1,66 +1,209 @@
 package com.example.yoursafetyandroid.menu;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.yoursafetyandroid.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HistoryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+
 public class HistoryFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HistoryFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HistoryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HistoryFragment newInstance(String param1, String param2) {
-        HistoryFragment fragment = new HistoryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private MapView mapView;
+    private MapboxMap map;
+    final Calendar myCalendar= Calendar.getInstance();
+    private EditText editText;
+    private TextView selectDate;
+    private FirebaseFirestore db;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        Mapbox.getInstance(MenuActivity.context, getString(R.string.mapbox_access_token));
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_history, container, false);
+        mapView = (MapView)  rootView.findViewById(R.id.mapViewHistory);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                map = mapboxMap;
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                    }
+                });
+            }
+        });
+
+        editText=(EditText)rootView.findViewById(R.id.dateHistory);
+        selectDate = (TextView)rootView.findViewById(R.id.selectDate);
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                updateLabel();
+                DocumentReference docRef = db
+                        .collection("history")
+                        .document(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
+                docRef.get().addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            Map<String, String> map2 = (Map) document.getData().get(editText.getText().toString().replace('/','-'));
+                            if(map2 != null)
+                            {
+                                map.clear();
+                                double latitude = 0;
+                                double longitude = 0;
+                                int i = 1;
+                                for (Map.Entry<String,String> entry : map2.entrySet()) {
+                                    String[] x = entry.getValue().split("-");
+                                    latitude = Double.parseDouble(x[0]);
+                                    longitude = Double.parseDouble(x[1]);
+                                    addMarkerToMap(latitude,longitude, entry.getKey(), i);
+                                    i++;
+                                }
+                                selectDate.setText("Below is the route!");
+                                selectDate.setTextColor(Color.BLACK);
+                            }
+                            else
+                            {
+                                selectDate.setText("There is no history!");
+                                selectDate.setTextColor(Color.RED);
+                            }
+                        }
+                    }
+                });
+            }
+        };
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(MenuActivity.context,date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        mapView.onStop();
+        editText.setText("");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory()
+    {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    private void updateLabel(){
+        String myFormat="MM/dd/yy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+        editText.setText(dateFormat.format(myCalendar.getTime()));
+    }
+
+    private void addMarkerToMap(double latitude, double longitude,String hour,int number)
+    {
+        MarkerOptions options  = new MarkerOptions();
+        if(number == 1) {
+            options.title("Start: " + hour);
+            options.icon(drawableToIcon(MenuActivity.context, R.drawable.start_history));
+            options.position(new LatLng(latitude, longitude));
+            CameraPosition position = new CameraPosition.Builder()
+                    .target(new LatLng(latitude, longitude)) // The location you want to zoom to
+                    .zoom(15) // The zoom level you want to set
+                    .build();
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(position), 3000); // 1000ms = 1s animation duration
+        }
+        else {
+                options.title(hour);
+                options.icon(drawableToIcon(MenuActivity.context, R.drawable.mid_history));
+                options.position(new LatLng(latitude, longitude));
+        }
+        map.addMarker(options);
+    }
+
+
+    public static Icon drawableToIcon(@NonNull Context context, @DrawableRes int id) {
+        Drawable vectorDrawable = ResourcesCompat.getDrawable(context.getResources(), id, context.getTheme());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        vectorDrawable.draw(canvas);
+        return IconFactory.getInstance(context).fromBitmap(bitmap);
     }
 }
--- app/src/main/java/com/example/yoursafetyandroid/menu/LocationFragment.java
+++ app/src/main/java/com/example/yoursafetyandroid/menu/LocationFragment.java
@@ -1,26 +1,47 @@
 package com.example.yoursafetyandroid.menu;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.yoursafetyandroid.R;
 import com.example.yoursafetyandroid.location.TimeLocation;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 
 public class LocationFragment extends Fragment {
@@ -46,7 +67,7 @@ public class LocationFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_location, container, false);
         mapView = (MapView)  rootView.findViewById(R.id.mapView2);
         mapView.onCreate(savedInstanceState);
-
+        mapView.invalidate();
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull MapboxMap mapboxMap) {
@@ -54,12 +75,26 @@ public class LocationFragment extends Fragment {
                 mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
+                        handler = new Handler();
+                        runnable = new RefreshData(map, handler);
+                        handler.postDelayed(runnable, 2);
+                        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+                        DocumentReference docRef = db.collection("liveLocation").document(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
+                        docRef.get().addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                if (document.exists()) {
+                                    CameraPosition position = new CameraPosition.Builder()
+                                            .target(new LatLng((double)document.getData().get("latitude"), (double)document.getData().get("longitude"))) // The location you want to zoom to
+                                            .zoom(13) // The zoom level you want to set
+                                            .build();
+                                    map.animateCamera(CameraUpdateFactory.newCameraPosition(position), 3000); // 1000ms = 1s animation duration
+                                }
+                            }
+                        });
                     }
                 });
-                handler = new Handler();
-                runnable = new RefreshData(map, handler);
-                handler.postDelayed(runnable, 2000);
             }
         });
         return rootView;
@@ -84,6 +119,7 @@ public class LocationFragment extends Fragment {
     {
         super.onStop();
         mapView.onStop();
+        handler.removeCallbacks(runnable);
     }
 
     @Override
@@ -112,13 +148,68 @@ public class LocationFragment extends Fragment {
         @Override
         public void run() {
             map.clear();
-            MarkerOptions options = new MarkerOptions();
-            options.title("current position");
-            options.position(new LatLng(45.760696+Math.random(),21.226788+Math.random()));
-            map.addMarker(options);
-            handler.postDelayed(this,2000);
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            DocumentReference docRef = db.collection("locationsPersons").document(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
+            docRef.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists() && document.getData().get("persons") != null) {
+                        List<String> persons = (ArrayList<String>)Objects.requireNonNull(document.getData().get("persons"));
+                        for (String person :persons) {
+                           addMarkerAndGetLocation(db, person);
+                        }
+                    }
+                }
+            });
+            addMarkerAndGetLocation(db, Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
+            handler.postDelayed(this,4000);
+
         }
     }
 
+    private void addMarkerAndGetLocation(FirebaseFirestore db,String personUid)
+    {
+        DocumentReference docRef = db.collection("liveLocation").document(personUid);
+        docRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists() && ((boolean)document.getData().get("shareLocation") || personUid ==  Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())) {
+                    addMarkerToMap(personUid,(double)document.getData().get("latitude"), (double)document.getData().get("longitude"),Integer.parseInt(document.getData().get("icon").toString()),(boolean) document.getData().get("danger"));
+                }
+            }
+        });
+    }
 
+    private void addMarkerToMap(String personUid, double latitude, double longitude,int number, boolean danger)
+    {
+        MarkerOptions options  = new MarkerOptions();
+        if(Objects.equals(personUid, Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())) {
+            options.title("Your location");
+           options.icon(drawableToIcon(MenuActivity.context,R.drawable.my_location_marker));
+           options.position(new LatLng(latitude, longitude));
+        }
+        else
+        {
+            options.position(new LatLng(latitude + Math.random(), longitude + Math.random()));
+            if (danger) {
+                options.title("I am in danger, i need help!!");
+                options.icon(drawableToIcon(MenuActivity.context,R.drawable.help_location));
+            }
+            else {
+                options.title(personUid);
+                options.icon(drawableToIcon(MenuActivity.context,R.drawable.people_location_marker));
+            }
+        }
+        map.addMarker(options);
+    }
+
+    public static Icon drawableToIcon(@NonNull Context context, @DrawableRes int id) {
+        Drawable vectorDrawable = ResourcesCompat.getDrawable(context.getResources(), id, context.getTheme());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        vectorDrawable.draw(canvas);
+        return IconFactory.getInstance(context).fromBitmap(bitmap);
+    }
 }
